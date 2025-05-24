13859cc89e3be3c23a2c870f3bcd1719f8702ea1
frankolien <Frankolien123@gmail.com>
2025-05-24 09:22:00 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/coffeeui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coffeeui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:coffeeui/screens/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: OnboardingScreen()),
+  );
+}
